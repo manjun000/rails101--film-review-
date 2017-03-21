6515f61abaf869ee0862fee819769f324cdfb1f5
manjun000 <manj34732@Gmail.com>
2017-03-21 16:59:32 +0800

    implement groups crud

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,45 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+
+      redirect_to groups_path
+    else
+      render :new
+    end
+ end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "Update Success"
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "已成功删除"
+    redirect_to groups_path
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+end
