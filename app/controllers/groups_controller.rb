ba3b7f20fb4fcae04eca527b0d3a592def4ccdd4
manjun000 <manj34732@Gmail.com>
2017-03-22 11:20:27 +0800

    gem devise step5-1 to step 5-5

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -9,8 +11,9 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    if @group.save
+    @group.user = current_user
 
+    if @group.save
       redirect_to groups_path
     else
       render :new
@@ -18,20 +21,20 @@ class GroupsController < ApplicationController
  end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
-    @group.update(group_params)
-    redirect_to groups_path, notice: "Update Success"
+    if  @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @group = Group.find(params[:id])
-    @group.destroy
-    flash[:alert] = "已成功删除"
-    redirect_to groups_path
+     @group.destroy
+      flash[:alert] = "已成功删除"
+      redirect_to groups_path
   end
 
   def show
@@ -41,5 +44,12 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:title, :description)
+  end
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 end
